e84a51a13ae1883f197648a28c22094fe41da9c8
Andrew <AJMulv9878@gmail.com>
2018-01-16 21:36:17 -0500

    a;ldkfjasd;lfkj

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -472,7 +472,7 @@ window.onload = function() {
 
 								}});
 
-								$('<input/>', {
+								$('<button/>', {
 										type: "submit",
 										id: "heroComics" + i,
 										class: "comicButton",
